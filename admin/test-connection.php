2a37af041efce617578a5f391caf40ce975c1b2e
Sampad Chandra <sampadchandra6@gmail.com>
2026-08-19 21:52:42 +0530

    feat: add PDO database connection

---
+++ admin/test-connection.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+
+echo "Database is successfully connected!";
+
+?>
---
+++ config/database.php
@@ -0,0 +1,35 @@
+<?php 
+/**
+ * Ssrini Handcrafts
+ * Database Configuretion
+ * 
+ * Creates a reusable PDO connection to MySQL/MariaDB.
+ */
+
+
+$host = '127.0.0.1';
+$dbname = 'ssrini_handcrafts';
+$username = 'root';
+$password = '';
+
+$charset = 'utf8mb4';
+$dsn = "mysql:host={$host};dbname={$dbname}; charset={$charset}";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   =>false,
+
+    ];
+
+try {
+    $pdo = new PDO(
+        $dsn,
+        $username,
+        $password,
+        $options
+    );
+
+} catch (PDOException $e) {
+    die('Database connection failed.');
+}
